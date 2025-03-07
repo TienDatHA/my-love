6bea5fa3f74d000707145c4bd4e5f984dd40ae96
Phạm Tiến Đạt <167111936+TienDatHA@users.noreply.github.com>
2025-03-08 01:19:50 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Chào người đẹp, anh viết trang web này chỉ muốn nói là. </br> ANH YÊU EM NHIỀUUUU!",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
+    "Receive my simple gift of good morning wrapped with sincerity, tied with care and sealed with a prayer to keep you safe and happy all day long! Take care!?",
+  buttonYes: "Đồng ý",
   buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+  titleModar: "Cảm ơn em.",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "Cảm ơn em vì đã cho anh cơ hội để anh có thể ở bên cạnh, che chở và bảo vệ em. </br> Anh biết anh không được đẹp trai, cũng chẳng lắm tài nhưng anh có một trái tim chân thành. </br> Em chính là cả thế giới của anh. </br>Tối nay anh sẽ đến đón em. Sẽ có một bất ngờ siêu to dành cho emmm. </br>Yêu em <3",
 };
